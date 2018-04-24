7ed31f7a7f916aab53cc48eccd1eb5b9620b21d3
monicasancho@outlook.com <monicasancho@outlook.com>
2018-04-24 15:33:56 +0200

    first commit

---
+++ entrypoint.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+# Update current user uid
+sed -i "s/${USERNAME}:x:[0-9][0-9]*:/${USERNAME}:x:`id -u`:/" /etc/passwd
+
+# SSH
+ssh-keygen -f /etc/ssh/ssh_host_rsa_key -N '' -t rsa && \
+ssh-keygen -f /etc/ssh/ssh_host_dsa_key -N '' -t dsa && \
+ssh-keygen -f /etc/ssh/ssh_host_ecdsa_key -N '' -t ecdsa && \
+ssh-keygen -f /etc/ssh/ssh_host_ed25519_key -N '' -t ed25519 && \
+/usr/sbin/sshd 
+
+# Wetty
+wetty -p ${WTY_PORT:-3000} --sshport ${SSH_PORT:-22} --sshuser ${USERNAME:-app} &
+
+# Extra Runs
+${RUNEXTRA:-echo "No extra runs"}
+
+# Default exec
+EXEC=${EXEC:-npm start --prefix /usr/src/node-red -- --userDir /data}
+
+# Exec ARGS or EXEC
+exec ${@:-$EXEC}
