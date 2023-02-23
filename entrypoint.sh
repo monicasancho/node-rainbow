ac632d57496e47fcd6d32f095d9d72b5691ad9ce
Monica Sancho <31369306+monicasancho@users.noreply.github.com>
2023-02-23 08:33:27 +0100

    removes ssh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,15 +1,5 @@
 #!/bin/sh
 
-# Update current user uid
-sed -i "s/${USERNAME}:x:[0-9][0-9]*:/${USERNAME}:x:`id -u`:/" /etc/passwd
-
-# SSH
-ssh-keygen -f /etc/ssh/ssh_host_rsa_key -N '' -t rsa && \
-ssh-keygen -f /etc/ssh/ssh_host_dsa_key -N '' -t dsa && \
-ssh-keygen -f /etc/ssh/ssh_host_ecdsa_key -N '' -t ecdsa && \
-ssh-keygen -f /etc/ssh/ssh_host_ed25519_key -N '' -t ed25519 && \
-/usr/sbin/sshd
-
 # Extra Runs
 ${RUNEXTRA:-echo "No extra runs"}
 
@@ -18,5 +8,3 @@ EXEC=${EXEC:-npm start --prefix /usr/src/node-red -- --userDir /data}
 
 # Exec ARGS or EXEC
 exec ${@:-$EXEC}
-
-
